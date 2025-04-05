37591265a8ecbe52f9696d6e0787d2b757e92f41
Danielhoyos06 <danielhoyos2006@gmail.com>
2025-04-05 00:02:33 -0500

    Update lanzador.pl

--- lanzador.pl
+++ lanzador.pl
@@ -9,35 +9,49 @@
 #                       con el proposito de hacer experimentos de caracter cientifico (fiablilidad y presion)
 #****************************************************************/
 
+# Obtiene el directorio actual y lo guarda en la variable $Path
 $Path = `pwd`;
-chomp($Path);
+chomp($Path);  # Elimina el salto de línea al final del string obtenido por `pwd`
 
-
+# Lista con los nombres de los ejecutables que se van a probar
 @Nombre_Ejecutable = ("mm_clasica");
 
+# Tamaños de las matrices con los que se va a probar
 @Size_Matriz = (500, 1000);
 
-@Num_Hilos = (1,2);
+# Cantidad de hilos que se utilizarán en las pruebas
+@Num_Hilos = (1, 2);
 
+# Número de repeticiones por cada combinación de parámetros
 $Repeticiones = 30;
 
+# Bucle para recorrer cada ejecutable
 foreach $Nombre_Ejecutable (@Nombre_Ejecutable) {
+
+    # Bucle para recorrer cada tamaño de matriz
     foreach $size (@Size_Matriz) {
+
+        # Bucle para recorrer cada número de hilos
         foreach $hilo (@Num_Hilos) {
 
-            $file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";
+            # Construye el nombre del archivo donde se almacenarán los resultados
+            $file = "$Path/$Nombre_Ejecutable-" . $size . "-Hilos-" . $hilo . ".dat";
 
+            # Ejecuta la prueba $Repeticiones veces
             for ($i = 0; $i < $Repeticiones; $i++) {
 
-                system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");
+                # Ejecuta el programa y redirige la salida al archivo .dat correspondiente
+                system("$Path/$Nombre_Ejecutable $size $hilo >> $file");
 
+                # Imprime en consola el comando que se está ejecutando
                 printf("$Path/$Nombre_Ejecutable $size $hilo \n");
             }
 
-            #close($file);
+            # Comentado: línea que sería para cerrar archivo si se usara filehandle
+            # close($file);
 
+            # Variable acumuladora que nunca se declara antes, pero se usa para contar combinaciones (opcional)
             $p = $p + 1;
         }
     }
 }
-
